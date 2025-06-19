625dcdddd42ae70eda83686283c1f99b487506be
dennistmz02 <56810887+dennistmz02@users.noreply.github.com>
2025-06-19 22:41:35 +0800

    update authentication and profile

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,7 @@
 # authentication/forms.py
 
 from django import forms
+from authentication.models import Profile  
 
 class LoginForm(forms.Form):
     email = forms.EmailField()
@@ -9,3 +10,23 @@ class LoginForm(forms.Form):
 class SignUpForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model  = Profile
+        fields = [
+            'age', 'gender', 'height_cm', 'sexual_orientation', 'pronouns',
+            'body_type', 'location', 'education_level', 'occupation',
+            'religion', 'ethnicity', 'politics', 'smoking', 'drinking',
+            'drug_use', 'has_kids', 'wants_kids', 'zodiac_sign',
+            'relationship_goals', 'hobbies', 'bio'
+        ]
+        widgets = {
+            # style every widget with Bootstrap classes
+            field: forms.TextInput(attrs={'class': 'form-control form-control-alternative'})
+            for field in fields
+        }
+        widgets.update({
+            'bio':     forms.Textarea(attrs={'rows': 4, 'class': 'form-control form-control-alternative'}),
+            'hobbies': forms.Textarea(attrs={'rows': 3, 'class': 'form-control form-control-alternative'})
+        })
