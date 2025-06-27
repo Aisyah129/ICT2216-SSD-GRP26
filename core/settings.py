3fb1aee10027f9652165ed2acbe2b19bd82fc4eb
dennistmz02 <56810887+dennistmz02@users.noreply.github.com>
2025-06-27 15:16:09 +0800

    update

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.129.248.34', 'localhost', '127.0.0.1']
 
 # Application definition
 
