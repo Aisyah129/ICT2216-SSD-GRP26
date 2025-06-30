6a3c1ace3072d73d9784c017acac54bcb1eaf0cd
Arcadiuz99 <145037769+Arcadiuz99@users.noreply.github.com>
2025-06-30 22:25:01 +0800

    Merge with the "merge/all" & Logging (Completed)
    Merged and updated in accordance with "merge/all" branch as of 30 June 2025
    
    - Added logging for key user actions (login, logout, registration, profile update, payment)
    - Displayed logs on admin dashboard with pagination and severity/email filtering
    - Included metadata and IP address tracking for audit

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
+#from django.contrib import admin
 from django.views.generic import TemplateView
-
+#from django.contrib.auth.decorators import login_required, user_passes_test
 from authentication.views import user_dashboard
+#from authentication.decorators import is_admin_user
+
+#admin.site.login = user_passes_test(is_admin_user)(admin.site.login)
 
 urlpatterns = [
+    #path("admin/", admin.site.urls),
     path('', include('authentication.urls')),  # root path handles login/register/dashboard
     path('browse/', user_dashboard, name='browse'),
     path('messages/', TemplateView.as_view(template_name='pages/messages.html'), name='messages'),
