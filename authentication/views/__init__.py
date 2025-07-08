596effa64d2f0f42f17c801d0f011cf3141ae338
Aisyah129 <aisyahnimanah12.9@gmail.com>
2025-07-09 01:06:21 +0800

    Reverted

--- authentication/views/__init__.py
+++
@@ -1,9 +0,0 @@
-# authentication/views/__init__.py
-
-from .auth_views import *
-from .profile_views import *
-from .match_views import *
-from .message_views import *
-from .billing_views import *
-from .report_views import *
-from .admin_views import *
